2efd6a2679d7882921750428bea75cc0c65f4be2
sealocal <local.mat@gmail.com>
2017-07-23 00:26:56 -0700

    add messages#index, #show, #new

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,19 @@
 class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
   def create
-    message = Message.create! params.require(:message).permit(:title, :content)
-    message.images.attach(params[:message][:images])
-    redirect_to message
+    @message = Message.create! params.require(:message).permit(:title, :content)
+    @message.images.attach(params[:message][:images])
+    redirect_to @message
   end
 end
